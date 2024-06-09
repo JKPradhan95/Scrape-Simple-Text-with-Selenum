0bfc23d13372cb8b824adb4628c17730e4129712
JKPradhan95 <168675593+JKPradhan95@users.noreply.github.com>
2024-06-09 09:21:29 +0000

    Scrape Simple Text with Selenum
    Scrape-Simple-Text-with-Selenum

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 def get_driver():
-  #set options to make browseing easier
-  options = webdriver.ChromeOptions()
-  options.add_argument("disable-infobars")
-  options.add_argument("start-maximized")
-  options.add_argument("disable-dev-shm-usage")
-  options.add_argument("no-sandbox")
-  options.add_experimental_option("excludeSwitches", ['enable-automation'])
-  options.add_argument("disable-blink-features=AutomationControlled")
-  
-  driver = webdriver.Chrome(options = options)
-  
-  driver.get("https://automated.pythonanywhere.com")
-  return driver
+    # Set options to make browsing easier
+    options = webdriver.ChromeOptions()
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("no-sandbox")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
 
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://automated.pythonanywhere.com/")
+    return driver
 
 def main():
-  driver = get_driver()
-  element = driver.find_element(by="xpath", value='/html/body/div[1]/div/h1[1]')
-  return element.text
+    driver = get_driver()
+    try:
+        element = driver.find_element(by="xpath", value='/html/body/div[1]/div/h1[1]')
+        return element.text
+    except NoSuchElementException:
+        return "Element not found"
+    finally:
+        driver.quit()
 
 print(main())
-
-
-
