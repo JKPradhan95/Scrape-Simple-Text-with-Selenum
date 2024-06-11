dc27402054839f189de3d86a5f402896789f03d3
JKPradhan95 <168675593+JKPradhan95@users.noreply.github.com>
2024-06-11 16:51:45 +0000

    code modified

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import time
 
-def get_driver():
-    # Set options to make browsing easier
+
+def get_driver(url):
     options = webdriver.ChromeOptions()
     options.add_argument("disable-infobars")
     options.add_argument("start-maximized")
     options.add_argument("disable-dev-shm-usage")
     options.add_argument("no-sandbox")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("excludeSwitches",["enable-automation"])
     options.add_argument("disable-blink-features=AutomationControlled")
 
     driver = webdriver.Chrome(options=options)
-    driver.get("https://automated.pythonanywhere.com/")
+    driver.get(url)
     return driver
 
 def main():
-    driver = get_driver()
-    try:
-        element = driver.find_element(by="xpath", value='/html/body/div[1]/div/h1[1]')
-        return element.text
-    except NoSuchElementException:
-        return "Element not found"
-    finally:
-        driver.quit()
+    inputurl = input("Enter the website here: ")
+
+    driver = get_driver(url=inputurl)
+
+    element = driver.find_element(by="xpath",value='//*[@id="content"]/div/div/div/div[1]/h2')
+
+    return element.text
 
 print(main())
